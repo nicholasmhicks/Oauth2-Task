1e6f81fbb826508a0ae98203fded60bbb1e5d8bd
Nicholas <101604238@student.swin.edu.au>
2018-09-03 14:44:04 +1000

    Add project files.

---
+++ Oauth2 Task/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Oauth2_Task.MvcApplication" Language="C#" %>
---
+++ Oauth2 Task/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Oauth2_Task.Startup))]
+namespace Oauth2_Task
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
